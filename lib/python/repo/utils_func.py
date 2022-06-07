3933c4ac53dccf9623e8b5b37d10fdde270a5c0c
admin <admin>
2022-06-08 09:44:39 +1000

    Git references update for DKU_TUTORIAL_AUTOMATION

---
+++ lib/python/repo/utils_func.py
@@ -0,0 +1,8 @@
+# some example utilities type functions to be included here; some data wrangling examples
+
+# test creating a function
+def test():
+  print("hello world")
+  
+def add(x):
+  print(x + 2)
